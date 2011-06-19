5a183c207d6d7210bcea4b5733ee71d0f1de0535
Koen Pellegrims <koen@bluebanana.be>
2011-06-19 16:39:56 +0200

    Added wait for phonegap

--- js/app.js
+++ js/app.js
@@ -1,23 +1,22 @@
-Ext.setup({
+Ext.regApplication({
+    name: 'arcc',
     icon: "images/icon.png",
     tabletStartupScreen: "tabletstartup.png",
     phoneStartupScreen: "images/Default.png",
     glossOnIcon: false,
-    onReady: function() {
-        console.log('Device Name: '     + device.name); 
-        console.log('Device PhoneGap: ' + device.phonegap); 
-        console.log('Device Platform: ' + device.platform); 
-        console.log('Device UUID: '     + device.uuid); 
-        console.log('Device Version: '  + device.version);
-	}
-});
-
-Ext.regApplication({
-    name: 'arcc',
     launch: function() {
         this.launched = true;
+        this.mainLaunch();
+    },
+    mainLaunch: function() {
+        if (!device || !this.launched) {return;}
+        console.log('mainLaunch');
+		
+		//remove splash image
+		var splash = Ext.get('splash-image');
+		splash.remove();
 		this.views.viewport = new arcc.views.Viewport();
-	}
+    }
 });
 
 
